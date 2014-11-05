ee1bbff5cb4e1c98bd0820c05bb21d70e80320d9
Juan Carlos Dominguez <juan.dominguez@esmas.net>
2014-11-05 17:18:13 -0600

    Prueba

--- app/views/urlSites/prueba.blade.php
+++ app/views/urlSites/prueba.blade.php
@@ -2,12 +2,14 @@
     @section('content')
 <br><br>
 <br>
+
 <div class="row"> 
     	<div id="editorfeeds" class="col-md-8">
     <section class="widget">
-        <header><h4><i class="fa fa-pencil"></i>  Editor de Video Clips</h4></header>
+        <header><h4><i class="fa fa-pencil"></i>  Editor </h4></header>
         <div class="body">
-            <form method="post">
+
+            <form method="post" id='formGen' class="form-horizontal" action="#">
                 <fieldset>
                     <div class="row">
                         <div class="col-sm-12">                        
@@ -18,83 +20,111 @@
                                             <div class="row">
                                                 <div class="col-md-10 col-md-offset-1">
 
-                                                    <legend class="section">Urls</legend>
-                                                    <form class="form-inline form-search no-margin text-align-center" role="form">
-                                                        <div class="input-group">
-                                                            <input id='inputUrls' class="form-control" placeholder="Urls">
+                                                    <legend class="section">Name Site</legend>
+                                                        <div class="input-group col-sm-10">
+                                                            <input id='inputUrls' class="form-control" placeholder="Urls" name='inputUrls'>
                                                         </div>
-                                                    </form>
-
-                                                      <legend class="section">Periodo</legend>
-                                                    <form class="form-inline form-search no-margin text-align-center" role="form">
-                                                            <div class="controls form-group">
-                                                                <div class="input-group col-sm-8">
-                                                                   
-                                                                    <span class="input-group-addon"><i class="fa fa-calendar-o"></i></span>Fecha
-                                                                    {{Form::text('birthdate','', array('id'=>'inputPeriod', 'class' => 'form-control date-picker','format'=>'yyyy/mm/dd','placeholder'=>'Periodo'))}}
-                                                                    <span class="input-group-btn"><a href="#" class="btn btn-danger " data-date-format="yyyy/mm/dd" data-date="today();">
-                                                                        <i class="fa fa-calendar"></i>
-                                                                        </a>
-                                                                    </span>
-
-                                                                    <span class="input-group-addon"><i class="eicon-clock"></i></span>Hora
-                                                                    {{Form::text('inputTimePicker','', array('id'=>'inputTime','class' => 'inputTimePicker','format'=>'hh:mm:ss','placeholder'=>'Tiempo'))}}
-                                                                    <script>$(".inputTimePicker").timepicki();</script>
-                                                                    <span class="input-group-btn "><a href="#" class="btn btn-danger" ><i class="eicon-clock"></i></a>
-                                                                    </span> 
-
-                                                                </div>
-                                                            </div>
-                                                    </form> 
 
 
-                                                    <legend class="section">Contenido</legend>
-                                                    <form class="form-horizontal form-search no-margin text-align-left" role="form">
-                                                        <div class="control-group">
+                                                    <legend class="section">Content Type</legend>
+                                                        <div class="control-group checkboxAlign">
                                                                 <label class="checkbox">
-                                                                    <input type="checkbox" id="checkbox1" class="iCheck"> Informaci&oacute;n
+                                                                    <input type="checkbox" name="contenido[]" class="iCheck contentCheck" value='Imagenes'> Imagenes
                                                                 </label>
                                                                 <label class="checkbox">
-                                                                    <input type="checkbox" id="checkbox2" class="iCheck"> Notas
+                                                                    <input type="checkbox" name="contenido[]" class="iCheck contentCheck" value='Notas'> Notas
                                                                 </label>
                                                                 <label class="checkbox">
-                                                                    <input type="checkbox" id="checkbox3" class="iCheck"> Videos
+                                                                    <input type="checkbox" name="contenido[]" class="iCheck contentCheck" value='Videos'> Videos
                                                                 </label>
                                                         </div>
-                                                    </form> 
+                                                    
+                                                   	
+                                                    <legend class="section">Period</legend>                                                 
+                                                        <div class=" col-sm-10">
+                                                                <select name="period"  class='chzn-select select-block-level' id='period'>
+									                                <option value="">............</option>
+									                                <option value='Hora'>Hora</option>
+                                                                	<option value='Dia'>Dia</option>
+                                                                	<option value='Semana'>Semana</option>
+                                                                	<option value='Mes'>Mes</option>
+                   												</select>   
+                                                        </div>
 
-                                                    <legend class="section">Sitio</legend>
-                                                    <form class="form-inline form-search no-margin text-align-center" role="form">
-                                                        <div class="input-group">
-                                                            <input id='inputSities'  class="form-control" placeholder="Sites">
+                                                    <legend class="section">Elements</legend>                                                 
+                                                       <div class=" col-sm-10">
+                                                               
+                                                        </div>
+
+                                                    <legend class="section">Incluir</legend>                                                
+                                                        <div class="input-group col-sm-10">
+                                                            <input id='inputSitiesInclude'  class="form-control" placeholder="Sites">
                                                             <span class="input-group-btn">
-                                                                <button type='button' class="btn btn-primary" id='addUrlSite'>
+                                                                <button type='button' class="btn btn-primary" id='addUrlSiteInclude'>
                                                                     Add
                                                                 </button>
                                                             </span>
                                                         </div>
-                                                    </form>     
+                                                
+                                                    <div class='body'>
+														<table id='formInclude' class="table table-striped no-margin sources-table">
+																<thead>
+																	<tr>
+																	<th>Include Url</th>
+																	</tr>
+																</thead>
+															
+															
+														</table>
+													</div>
+                                                    <legend class="section">Excluir</legend>
+                                                        <div class="input-group col-sm-10">
+                                                            <input id='inputSitiesExclude'  class="form-control" placeholder="Sites">
+                                                            <span class="input-group-btn">
+                                                                <button type='button' class="btn btn-primary" id='addUrlSiteExclude'>
+                                                                    Add
+                                                                </button>
+                                                            </span>
+                                                        </div>
+                                                  
+                                                    <div class='body'>
+
+														<table id='formExclude' class="table table-striped no-margin sources-table">
+																<thead>
+																	<tr>
+																		<th>Exclude Url</th>
+																	</tr>
+																</thead>
+															
+															
+														</table>
+													</div>     
                                                 </div>
                                             </div>
                                         </div>
                                     </section>
                                 </div>                             
-                           
-                           <!-- <div class="form-actions">                                           
-                                    <button id='AddNewUrl' type="button"  class="btn btn-warning"><i class="fa fa-plus"></i>  Agregar Nuevo </button>
-                                    <button id="saveVideoClips"    type="button"  class="btn btn-default"><i class="fa fa-refresh"></i>  Refrescar</button>
-                            </div>   -->                                                     
-                           
                             </div>
                         </div>
                     </div>    
                 </fieldset>
             </form>
+
+            			<div class="well well-sm well-white">
+                            <div class="row">
+                                <div class="col-xs-5 col-sm-offset-5">
+                                    <button type="button" class="btn btn-primary btn-sm" data-placement="top" id='saveUrl'>
+                                        Save
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
         </div>
     </section>
 </div>  
 
 </div>
+
 
 @stop
 
@@ -104,52 +134,85 @@
  @parent
 		{{ HTML::script('/light-blue/lib/bootstrap-datepicker.js') }}
         {{ HTML::script('js/formAccountNewUpd.js') }} 
-        <script src="/js/timepicki.js"></script> 
+        {{ HTML::script('light-blue/lib/icheck.js/jquery.icheck.js') }}
+        <!--<script src="/js/timepicki.js"></script> -->
 
+<script src="/light-blue/lib/select2.js"></script>
+<script src="/light-blue/lib/bootstrap/collapse.js"></script>
+<script src="/light-blue/lib/bootstrap/transition.js"></script>
+<script src="/light-blue/lib/bootstrap/button.js"></script>
+<script src="/light-blue/lib/bootstrap-select/bootstrap-select.js"></script>
+
+
+
+<style type="text/css">
+	table th,td{padding: .5em;}
+	select{color:black;}
+	.checkboxAlign label{float: left;padding-left: 4em;}
+	td input{color:black;}
+
+
+</style>
 
         <script type="text/javascript">
-/*
-|-------------------------------------------------------------------------------
-| Variables Globales
-|-------------------------------------------------------------------------------
-|
-*/ 
-var inputUrls = $('#inputUrls');
-var inputSities = $('#inputSities');
-var checkbox1 = $('#checkbox1');
-var inputPeriod = $('#inputPeriod');
-var inputTime = $('#inputTime');
-//var AddNewUrl = $('#AddNewUrl');    
-/*
-|-------------------------------------------------------------------------------
-| escaleta/displayCounterFeeds
-|-------------------------------------------------------------------------------
-|
-*/ 
-$('#addUrlSite').on('click',function(event) {  
 
+/*var inputUrls = $('#inputUrls').val(),
+	period = $("#period").val(),
+	arrayContent = [];
 
+	$("input[type=checkbox]:checked").each(function() {
+	        arrayContent.push($(this).val());
+	 });
+	arrayContent.toString();
+*/
 
-    alert("hola");
-
-    /*inputUrls = $('#inputUrls').val();
-    inputSities = $('#inputSities').val();
-    checkbox1 = $('#checkbox1').val();
-    inputPeriod = $('#inputPeriod').val();
-    
-    $.ajax({
-        url: 'home/addNewUrl',
-        type: 'get',
-        data: {'inputUrls': inputUrls,'inputSities':inputSities,'checkbox1':checkbox1,'inputPeriod':inputPeriod},
-        success: function(data) {
-//           if (data) {
-//              $('#counterFeeds').html(data);
-//           } else {
-//              $('#counterFeeds').html('<div>0</div>');
-//           }
-//           attachevent();
-        }
-     });*/
+$(".iCheck").iCheck({
+            checkboxClass: 'icheckbox_square-grey',
+            radioClass: 'iradio_square-grey'
 });
+
+$(".chzn-select").each(function(){
+            $(this).select2($(this).data());
+});  
+
+$('#addUrlSiteInclude').on('click',function() {  
+
+	var inputSitiesInclude = $('#inputSitiesInclude').val();
+
+	if(inputSitiesInclude){
+		$("#formInclude > thead").append($("<tr>")
+									.append($("<td>")
+										.append($("<input>",{value:inputSitiesInclude,name:'sitesInclude[]',text:inputSitiesInclude}))));
+		
+		$("#inputSitiesInclude").val('');
+	}
+
+		
+
+
+});
+
+$('#addUrlSiteExclude').on('click',function() {  
+
+	var inputSitiesExclude = $('#inputSitiesExclude').val();
+
+		if(inputSitiesExclude){
+
+			$("#formExclude > thead").append($("<tr>")
+									.append($("<td>").addClass("col-sm-10")
+										.append($("<input>",{value:inputSitiesExclude,name:'sitesExclude[]',text:inputSitiesExclude}))));
+
+			$("#inputSitiesExclude").val('');
+		}	
+});
+
+$("#saveUrl").on('click',function(){
+	console.log($("#formGen").serialize())
+});
+
+
+
+
 </script>
 @stop
+
